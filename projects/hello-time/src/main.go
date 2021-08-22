017c1c63e1e2d5983032b9899580e77e11b51a38
Takács Ákos <rimelek@rimelek.hu>
2021-08-22 12:13:08 +0200

    reformat long line in hello-time

--- projects/hello-time/src/main.go
+++ projects/hello-time/src/main.go
@@ -4,5 +4,8 @@ import "time"
 
 func main() {
 	loc, _ := time.LoadLocation("Europe/Budapest")
-	println("Hello Go!", time.Date(2021, time.August, 20, 12, 0, 0, 0, loc).Format(time.RFC3339))
+	println(
+		"Hello Go!",
+		time.Date(2021, time.August, 20, 12, 0, 0, 0, loc).Format(time.RFC3339),
+	)
 }
